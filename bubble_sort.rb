83b2d8f566b73b3c3ec70691f7fb35e2de2b4339
Chris <csalois@gmail.com>
2019-07-29 12:08:30 -0400

    made bubble_sort and bubble_sort_by one file

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -12,3 +12,20 @@ def bubble_sort(array)
 end
 
 bubble_sort([4,3,78,2,0,2])
+
+def bubble_sort_by(array)
+  (array.length - 1).times do |times|    
+    (array.length - times).times do |index|
+      if array[index + 1]
+        if yield(array[index], array[index +1]) > 0
+          array[index], array[index + 1] = array[index + 1], array[index]
+        end
+      end
+    end
+  end 
+  array 
+end
+  
+bubble_sort_by(["hi","hello","hey"]) do |left,right|
+  left.length - right.length
+end
